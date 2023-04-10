eff3b410a0190b6542623e4b6910612f727ef081
Maria Garcia <mgarcia18@hawk.iit.edu>
2023-04-10 14:43:58 -0500

    gets rows, need to write to CSV

--- file_splitter.py
+++ file_splitter.py
@@ -7,8 +7,56 @@ def string_to_csv(input, filename):
     with open(filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
 
-        current_row = values[:13]
-        print("\n current row", current_row)
+        next_precinct = ""
+
+        while(len(values) >= 13):
+            if(next_precinct):
+                current_row = [next_precinct] + values[:12]
+                last_val = values[12][:-4]
+                next_precinct = values[12][-4:]
+                values = values[13:]
+            else:
+                #for first iteration
+                current_row = values[:13]
+                last_val = values[13][:-4]
+                next_precinct = values[13][-4:]
+                values = values[14:]
+                current_row = current_row + [last_val]
+            
+            print("next precinct: ", next_precinct)
+            print("last_value: ", last_val)
+            print(current_row)
+
+            current_row.append(last_val)
+
+
+
+       
+
+
+
+        # for i in range(0, len(values)):
+        #     current_row = values[:13]
+        #     if i % 14 == 13:
+        #         last_four_chars = values[i][-4:]
+        #         first_part = values[i][:-4]
+
+        #         print("precinct: ", last_four_chars)
+        #         print("last_value: ", first_part)
+
+               
+        #         current_row.append(first_part)
+        #         csvwriter.writerow(current_row)
+
+              
+        #         csvwriter.writerow([last_four_chars])
+
+        #         current_row = []
+        #     else:
+        #         current_row.append(values[i])
+
+        # if current_row:
+        #     csvwriter.writerow(current_row)
 
     print("\n --- success ---")
 
